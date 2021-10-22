9a68a4761406d1b4e80fa0102cf8347665b8d7a4
James <jimblackwood3@gmail.com>
2021-10-22 16:45:30 -0500

    add description

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,6 +4,7 @@
         Dry Gas MBAL
     </h1>
     <img alt="MBAL-62" src="../assets/reservoir.png" width="400" height="200">
+    <p> Provide pressure and production profile for a dry gas field to yield OGIP estimates using both P/Z and F vs. Eg plots. </p>
     </div>
 </template>
 
